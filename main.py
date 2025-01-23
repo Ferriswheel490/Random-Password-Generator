80318b3f662f41058d34b023beb8ce889a113d1d
Ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-01-23 13:25:04 -0700

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 #Fairus De La Cruz Random Password Generator
 
-def main():
-    how_long_is_the_password = input("how long is your password: ")
 
+'''''
+A function for password
+like how long it should be
+letters in lower and uppercase
+numbers and special charaters
+'''''
+def main():
+    x = input('how long is your password:')
+    print(x)
+    does_it_need_letters = "does it need letters"
+    if does_it_need_letters == True:
+        do_you_want_upper__lower_or_both = input("do you want upper, lower, or both:")
+    else:
+        print("they need both")
+    special_charaters = input()
 def running():
     pass
